8976470f45c3e6e8992b7e5219eea661c470978d
and-cuau <98066177+and-cuau@users.noreply.github.com>
2025-06-05 22:15:36 -0400

    use env var for redis connection

--- my-server-sql/flow_parent_queue .js
+++ my-server-sql/flow_parent_queue .js
@@ -2,7 +2,7 @@
 const { Queue } = require("bullmq");
 const IORedis = require("ioredis");
 
-const connection = new IORedis({
+const connection = new IORedis(process.env.REDIS_URL ||{
   host: "127.0.0.1",
   port: 6379,
   maxRetriesPerRequest: null,
