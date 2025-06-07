40ef7d8fa1decd86d50c795108755190de9e41ec
and-cuau <98066177+and-cuau@users.noreply.github.com>
2025-06-07 18:35:57 -0400

    Hard coded REACT_APP_API_URL instead of using service variable

--- client/src/components/LogIn.tsx
+++ client/src/components/LogIn.tsx
@@ -15,7 +15,7 @@ type MissionProgressProps = {
   role: string;
 };
 
-const API_URL = process.env.REACT_APP_API_URL;
+const API_URL = process.env.REACT_APP_API_URL || "https://amiable-caring-production.up.railway.app";
 // const API_URL = "http://localhost:3000";
 
 export default function LogIn({ role }: MissionProgressProps) {
