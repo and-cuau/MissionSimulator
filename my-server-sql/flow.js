645f4d7b6d5c396c887ee56cdbf982b494b44091
and-cuau <98066177+and-cuau@users.noreply.github.com>
2025-06-06 23:23:19 -0400

    set host, port, etc individually to process.env.redis_host

--- my-server-sql/flow.js
+++ my-server-sql/flow.js
@@ -2,13 +2,12 @@ const { FlowProducer } = require("bullmq");
 
 const IORedis = require("ioredis");
 
-const connection = new IORedis(
-  process.env.REDIS_URL || {
-    host: "127.0.0.1",
-    port: 6379,
-    maxRetriesPerRequest: null,
-  },
-);
+const connection = new IORedis({
+  host: process.env.REDIS_HOST || "127.0.0.1",
+  port: parseInt(process.env.REDIS_PORT) || 6379,
+  password: process.env.REDIS_PASSWORD || undefined,
+  maxRetriesPerRequest: null,
+});
 
 const flow = new FlowProducer({ connection });
 
